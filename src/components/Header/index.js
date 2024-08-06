681c80c317232e8668ec77342dc08f799ec16b47
zaurmurtaza63 <shahnawazabdulrehman@gmail.com>
2024-08-07 01:43:34 +0500

    header animation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,91 +1,83 @@
-
 "use client";
-import React, { useState } from 'react'
-import './style.css'
-import Image from 'next/image'
+import React, { useState } from 'react';
+import './style.css';
+import Image from 'next/image';
 import logo from '../../../public/assets/logo.svg';
 import profile from '../../../public/assets/profile.png';
 import menuicon from '../../../public/assets/menu-icon.svg';
-
+import { motion } from 'framer-motion';
 
 export default function Header() {
+  const [headerOpen, setHeaderOpen] = useState(false);
 
-  const [headerOpen, setHeaderOpen] = useState(false)
   return (
     <>
-
-
-
-      <header className='header w-full  md:mt-2 sm:mt-2 engine-header mb-16 '>
+      <header className='header w-full md:mt-2 sm:mt-2 engine-header mb-16'>
         <div className='container'>
-        <div className='flex justify-between items-center'>
-          <div className='logo  sm:w-40 lg:w-auto'>
-            <Image src={logo} />
-          </div>
-          <div className='headerDefault headerMenu py-2 xl:py-5 xl:px-16 xl:px-16 md:px-6 sm:px-3'>
-            <ul className='flex text-white xl:gap-12 md:gap-3 lg:gap-6 gap-4'>
-              <li><a href="https://drive.google.com/file/d/1ZSMbAvM8ulRg9-saG0uZ-cZ8z44dsjvj/view" className='menu-links uppercase'>White-paper</a></li>
-              <li><a href="https://drive.google.com/file/d/1ZSMbAvM8ulRg9-saG0uZ-cZ8z44dsjvj/view" className='menu-links uppercase'>VISUAL NOVEL</a></li>
-              <li><a href="#" className='menu-links uppercase'>INITIAL DEMO</a></li>
-            </ul>
-          </div>
-          {headerOpen &&
-          
-          <div className='headerResponsive headerMenu py-2 xl:px-16 xl:px-16 md:px-6 sm:px-3'>
-            <ul className='flex text-white xl:gap-12 md:gap-3 lg:gap-6 gap-4'>
-              <li><a href="https://drive.google.com/file/d/1ZSMbAvM8ulRg9-saG0uZ-cZ8z44dsjvj/view" className='menu-links uppercase'>White-paper</a></li>
-              <li><a href="https://drive.google.com/file/d/1ZSMbAvM8ulRg9-saG0uZ-cZ8z44dsjvj/view" className='menu-links uppercase'>VISUAL NOVEL</a></li>
-              <li><a href="#" className='menu-links uppercase'>INITIAL DEMO</a></li>
-            </ul>
-          </div>
-          }
-          <div className='headerbtn-box flex items-center lg:gap-3 md:gap-1'>
-            <div className='left-line h-4 w-0.5 bg-black'></div>
-            <div className='header-btn py-1.5 lg:py-2 px-3 xl:py-3.5 lg:px-4 md:px-2 text-white Oxanium '><a href="#" className='font-bold'>Connect Wallet</a></div>
-            <div className='right-line h-4 w-0.5'></div>
-            <div className='profile-img hidden pr-2'>
-              <Image src={profile} />
-            </div>
-            <input type='checkbox' id='check'></input>
-            <label for="check" className='checkbtn block sm:hidden pl-4'>
-              <div className='menu-icon pr-3' onClick={()=>{
-                setHeaderOpen(!headerOpen)
-              }}>
-                <Image src={menuicon} />  
+          <div className='flex justify-between items-center'>
+            <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, delay: 0.2 }}
+            className='logo sm:w-40 lg:w-auto'>
+              <Image src={logo} alt="Logo" loading="lazy" />
+            </motion.div>
+            <motion.div
+            initial={{ opacity: 0, y: -30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, delay: 0.6 }}
+             className='headerDefault headerMenu py-2 xl:py-5 xl:px-16 xl:px-16 md:px-6 sm:px-3'>
+              <ul className='flex text-white xl:gap-12 md:gap-3 lg:gap-6 gap-4'>
+                <li>
+                  <a href="https://drive.google.com/file/d/1ZSMbAvM8ulRg9-saG0uZ-cZ8z44dsjvj/view" className='menu-links uppercase'>White-paper</a>
+                </li>
+                <li>
+                  <a href="https://drive.google.com/file/d/1ZSMbAvM8ulRg9-saG0uZ-cZ8z44dsjvj/view" className='menu-links uppercase'>VISUAL NOVEL</a>
+                </li>
+                <li>
+                  <a href="#" className='menu-links uppercase'>INITIAL DEMO</a>
+                </li>
+              </ul>
+            </motion.div>
+            {headerOpen &&
+              <div className='headerResponsive headerMenu py-2 xl:px-16 xl:px-16 md:px-6 sm:px-3'>
+                <ul className='flex text-white xl:gap-12 md:gap-3 lg:gap-6 gap-4'>
+                  <li>
+                    <a href="https://drive.google.com/file/d/1ZSMbAvM8ulRg9-saG0uZ-cZ8z44dsjvj/view" className='menu-links uppercase'>White-paper</a>
+                  </li>
+                  <li>
+                    <a href="https://drive.google.com/file/d/1ZSMbAvM8ulRg9-saG0uZ-cZ8z44dsjvj/view" className='menu-links uppercase'>VISUAL NOVEL</a>
+                  </li>
+                  <li>
+                    <a href="#" className='menu-links uppercase'>INITIAL DEMO</a>
+                  </li>
+                </ul>
               </div>
-            </label>
+            }
+            <motion.div
+            initial={{ opacity: 0, y: -40 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, delay: 0.8 }}
+
+               className='headerbtn-box flex items-center lg:gap-3 md:gap-1'>
+              <div className='left-line h-4 w-0.5 bg-black'></div>
+              <div className='header-btn py-1.5 lg:py-2 px-3 xl:py-3.5 lg:px-4 md:px-2 text-white Oxanium'>
+                <a href="#" className='font-bold'>Connect Wallet</a>
+              </div>
+              <div className='right-line h-4 w-0.5'></div>
+              <div className='profile-img hidden pr-2'>
+                <Image src={profile} alt="Profile" loading="lazy" />
+              </div>
+              <input type='checkbox' id='check'></input>
+              <label htmlFor="check" className='checkbtn block sm:hidden pl-4'>
+                <div className='menu-icon pr-3' onClick={() => setHeaderOpen(!headerOpen)}>
+                  <Image src={menuicon} alt="Menu Icon" loading="lazy" />
+                </div>
+              </label>
+            </motion.div>
           </div>
         </div>
-        </div>
-
       </header>
-
-
-      {/* <footer className='w-full relative'>
-    <div className='footer-top-line w-full'>
-    <Image className='w-full' src={footerline} /> 
-    </div>
-      <div className='footer-content flex items-center justify-around my-14 md:my-24 flex-col gap-8 md:flex-row'>
-        <div>
-        <ul className='flex footer-list xl:gap-12 md:gap-4 lg:gap-6'>
-        <li><a href="#" className='xs uppercase footer-gray-text'>White-paper</a></li>
-        <li><a href="#" className='xs uppercase footer-gray-text'>VISUAL NOVEL</a></li>
-        <li><a href="#" className='xs uppercase footer-gray-text'>INITIAL DEMO</a></li>
-        </ul>
-        </div>
-        <div className='flex items-center gap-6'>
-        <a href="#" className='follow-us uppercase text-white'>Follow us</a>
-        <a href="#"><Image className='footer-social-img' src={facebook} /></a>
-        <a href="#"><Image className='footer-social-img' src={cam} /></a>
-        <a href="#"><Image className='footer-social-img' src={linkedin} /></a>
-        </div>
-        <div><div className='footer-btn py-2 lg:px-4 md:px-2 text-white'><button className='flex'><a href="#" className='footer-btn-text'>Connect Wallet</a></button></div>
-        </div>
-      </div>
-      <div className='footer-copty-text py-1'>
-        <p>© Copyright © 2024 Incentives  Incentives - AI Automations and blockchain sales@idea-engine.ai</p>
-      </div>
-   </footer> */}
     </>
   );
 }
--- src/components/firstSection/index.js
+++ src/components/firstSection/index.js
@@ -10,58 +10,56 @@ import animationData from '../../lotties/man';
 import lottieBg from '../../../public/assets/lottie-bg.webp';
 
 export default function FirstSection() {
-    return (
-        <div className='container xl:mb-32'>
-            <div className='flex-wrap text-white flex md:row flex-col-reverse sm:flex-row gap-2 sm:gap-0'>
-            <motion.div
-    initial={{ opacity: 0, x: -20 }}
-    animate={{ opacity: 1, x: 0 }} 
-    transition={{ duration: 0.6, delay: 0.2 }} 
-    className='w-[100%] sm:w-[50%]'
->
-
-                    <motion.h2
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        transition={{ duration: 0.6, delay: 0.4 }}
-                        className='hero_title Oxanium md:text-start font-bold text-2xl mt-10 md:text-3xl lg:text-4xl xl:text-5xl mb-5 xl:mb-10 md:w-[97%]'
-                    >
-                        AI - WEARABLE CRYPTO COLLECTIBLES ECOSYSTEM
-                    </motion.h2>
-                    <motion.p
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        transition={{ duration: 0.6, delay: 0.6 }}
-                        className='LatoLight xl:text-xl md:text-start'
-                    >
-                        Gamified DePin + AI companion System on wearable
-                    </motion.p>
-                    <motion.div
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        transition={{ duration: 0.6, delay: 0.8 }}
-                        className='flex items-center md:items-start md:justify-start my-10 md:mb-0 xl:mt-20'
-                    >
-                        <Button title="Private Sale" />
-                        <Button title="White List" />
-                    </motion.div>
-                </motion.div>
-                <motion.div
-                    initial={{ opacity: 0, x: 20 }}
-                    animate={{ opacity: 1, x: 0 }} 
-                    transition={{ duration: 0.6, delay: 0.2 }} 
-              
-                    className='sm:w-[50%] flex items-center justify-center w-100 lottie-column'
-                >
-                    <Image src={lottieBg} layout='responsive' alt="Lottie Background" />
-                    <Player
-                        autoplay
-                        loop
-                        src={animationData}
-                        style={{ height: '400px', width: '400px', opacity: "75%" }}
-                    />
-                </motion.div>
-            </div>
-        </div>
-    );
+  return (
+    <div className='container xl:mb-32'>
+      <div className='flex-wrap text-white flex md:row flex-col-reverse sm:flex-row gap-2 sm:gap-0'>
+        <motion.div
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.6, delay: 0.2 }}
+          className='w-[100%] sm:w-[50%]'
+        >
+          <motion.h2
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.6 }}
+            className='hero_title Oxanium md:text-start font-bold text-2xl mt-10 md:text-3xl lg:text-4xl xl:text-5xl mb-5 xl:mb-10 md:w-[97%]'
+          >
+            AI - WEARABLE CRYPTO COLLECTIBLES ECOSYSTEM
+          </motion.h2>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.6}}
+            className='LatoLight xl:text-xl md:text-start'
+          >
+            Gamified DePin + AI companion System on wearable
+          </motion.p>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.6 }}
+            className='flex items-center md:items-start md:justify-start my-10 md:mb-0 xl:mt-20'
+          >
+            <Button title="Private Sale" />
+            <Button title="White List" />
+          </motion.div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, x: 20 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.6, delay: 0.2 }}
+          className='sm:w-[50%] flex items-center justify-center w-100 lottie-column'
+        >
+          <Image src={lottieBg} layout='responsive' alt="Lottie Background" loading="lazy" />
+          <Player
+            autoplay
+            loop
+            src={animationData}
+            style={{ height: '400px', width: '400px', opacity: "75%" }}
+          />
+        </motion.div>
+      </div>
+    </div>
+  );
 }
