ea889f35a09f6ea4b5a77a203193aca242e382e6
Muhammad Shahnawaz <shahnawazabdulrehman@gmail.com>
2024-08-19 16:51:11 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,13 +5,29 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "IDEA Engine: AI - wearable crypto collectibles ecosystem",
-  description: "IDEA Engine: AI - wearable crypto collectibles ecosystem",
+  description: "Explore the IDEA Engine, an innovative ecosystem where AI meets wearable crypto collectibles. Unlock new dimensions of digital ownership and creativity.",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <>
+    <html>
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VTKRRHY6PQ"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-VTKRRHY6PQ');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>{children}</body>
-    </html>
+      </html>
+    </>
   );
 }
